85841b9b906c9f6f7b4a01e15b7e89854b8ee182
Tien Dat Huynh <huynhztienzdat@gmail.com>
2024-10-25 23:10:16 +0700

    chore: 📐 update project settings

--- eslint.config.js
+++ eslint.config.js
@@ -34,7 +34,7 @@ module.exports = tseslint.config(
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: '',
           style: 'camelCase',
         },
       ],
