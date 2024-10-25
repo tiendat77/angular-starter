46770c54a6747048a67babaf7b225c7a7a57f19a
Tien Dat Huynh <huynhztienzdat@gmail.com>
2024-10-25 21:48:12 +0700

    feat: ✨ not found page

---
+++ src/app/features/not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {}
