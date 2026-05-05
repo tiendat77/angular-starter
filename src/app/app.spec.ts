35c46e2ce7996a1191a7304b24886920e843f15a
Tien Dat Huynh <huynhztienzdat@gmail.com>
2026-05-05 10:07:15 +0700

    feat: ✨ setup vitest

---
+++ src/app/app.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { describe, expect, it } from 'vitest';
+
+describe('Initial Setup', () => {
+  it('should work with Vitest', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should have TestBed initialized', () => {
+    expect(TestBed).toBeDefined();
+  });
+});
